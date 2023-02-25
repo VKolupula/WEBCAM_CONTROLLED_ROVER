2f42aebba85ca85be11503c99e0faf5de05c1f90
Vikram Kolupula <vikrampatelkolupula@gmail.com>
2023-02-25 18:07:28 +0000

    update

---
+++ Task_0/simulink_rover_control_ert_rtw/simulink_rover_control_data.c
@@ -0,0 +1,89 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: simulink_rover_control_data.c
+ *
+ * Code generated for Simulink model 'simulink_rover_control'.
+ *
+ * Model version                  : 1.29
+ * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+ * C/C++ source code generated on : Sat Feb 25 16:40:47 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "simulink_rover_control.h"
+
+/* Block parameters (default storage) */
+P_simulink_rover_control_T simulink_rover_control_P = {
+  /* Expression: -1
+   * Referenced by: '<Root>/Battery Read'
+   */
+  -1.0,
+
+  /* Expression: 1
+   * Referenced by: '<Root>/Constant'
+   */
+  1.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/Servo enable'
+   */
+  0.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/Encoder enable'
+   */
+  0.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/PWM'
+   */
+  0.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/Motor enable'
+   */
+  0.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/PWM1'
+   */
+  0.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/Motor enable1'
+   */
+  0.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/On//Off LED'
+   */
+  0.0,
+
+  /* Computed Parameter: Out1_Y0
+   * Referenced by: '<S1>/Out1'
+   */
+  0,
+
+  /* Computed Parameter: Out2_Y0
+   * Referenced by: '<S1>/Out2'
+   */
+  0,
+
+  /* Computed Parameter: ADCValuetoVoltageConversion_Gai
+   * Referenced by: '<Root>/ADC Value to Voltage Conversion'
+   */
+  34440U
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
